b75b69249eda0d7bbbd1be58dc8b6e43cffc656e
406123MatiasCuelloP <406123@tecnicatura.frc.utn.edu.ar>
2024-09-16 18:55:04 -0300

    Template

---
+++ Gestion_Acceso/src/main/java/com/example/Gestion_Acceso/GestionAccesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_Acceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAccesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAccesoApplication.class, args);
+	}
+
+}
---
+++ Gestion_Acceso/src/test/java/com/example/Gestion_Acceso/GestionAccesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_Acceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAccesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
