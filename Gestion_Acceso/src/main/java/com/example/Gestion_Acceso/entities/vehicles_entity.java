276944235f4efe5855dc4b1fd950c891dbb74a9d
Federico Gonzalez <405516@tecnicatura.frc.utn.edu.ar>
2024-09-17 16:12:20 -0300

    vehicle_entity

---
+++ Gestion_Acceso/src/main/java/com/example/Gestion_Acceso/entities/vehicles_entity.java
@@ -0,0 +1,28 @@
+package com.example.Gestion_Acceso.entities;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "vehicles")
+public class Vehicles_types_entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "vehicle_type_id")
+    private Integer vehicleTypeId;
+
+    private String plate;
+
+    private String insurace;
+
+    @Column(name = "created_user")
+    private Integer createdUser;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "last_updated_user")
+    private Integer lastUpdatedUser;
+
+    @Column(name = "last_updated_date")
+    private Date lastUpdatedDate;
+}
