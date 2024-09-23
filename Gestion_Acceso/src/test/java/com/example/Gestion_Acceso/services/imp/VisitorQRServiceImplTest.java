c1777f63fea404a5e43995a834334bb0cf93884d
405516_Gonzalez_Federico <405516@tecnicatura.frc.utn.edu.ar>
2024-09-23 20:15:30 -0300

    test visitorQrServiceImpl

---
+++ Gestion_Acceso/src/test/java/com/example/Gestion_Acceso/services/imp/VisitorQRServiceImplTest.java
@@ -0,0 +1,111 @@
+package com.example.Gestion_Acceso.services.imp;
+
+import com.example.Gestion_Acceso.entities.QRCode_Entity;
+import com.example.Gestion_Acceso.entities.Users_AllowedEntity;
+import com.example.Gestion_Acceso.models.*;
+import com.example.Gestion_Acceso.repositories.QRCodeRepository;
+import com.example.Gestion_Acceso.services.VisitorQRService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class VisitorQRServiceImplTest {
+    @SpyBean
+    private VisitorQRService visitorQRService;
+    @MockBean
+    private QRCodeRepository qrCodeRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @SneakyThrows // por la linea 86 por que object mapper (el metodo writeValueAsString) puede lanzar una exepcion no controlada JsonProcessingException
+    @Test
+    void generateAndSaveQRForVisitor()  {
+        Visitors visitors=new Visitors();
+        UserAllowed userAllowed=new UserAllowed();
+
+        userAllowed.setName("Fede");
+        userAllowed.setDocument("1111");
+
+        List<AuthRange> authRanges=new ArrayList<>();
+        AuthRange ar=new AuthRange();
+        ar.setNeighbor_Id(7);
+        ar.setId(1l);
+        LocalDate fecha1=LocalDate.now();
+        LocalDate fecha2=LocalDate.of(2024,9,24);
+        ar.setInitDate(fecha1);
+        ar.setEndDate(fecha2);
+        authRanges.add(ar);
+        userAllowed.setAuthRanges(authRanges);
+
+
+        VehicleType vehicleType =new VehicleType();
+        vehicleType.setId(1l);
+        vehicleType.setDescription("dni");
+        List<Vehicle> vehicleList=new ArrayList<>();
+        Vehicle v=new Vehicle();
+        v.setId(1l);
+        v.setPlate("ag-435-f");
+        v.setVehicleTypeId(vehicleType);
+        vehicleList.add(v);
+        userAllowed.setVehicles(vehicleList);
+
+        DocumentType documentType=new DocumentType();
+        documentType.setDescription("Dni");
+        userAllowed.setDocumentType(documentType);
+
+        visitors.setUserAllowed(userAllowed);
+
+        QRCodeData qrCodeData = new QRCodeData();
+        qrCodeData.setNeighborId(visitors.getUserAllowed().getAuthRanges().get(0).getNeighbor_Id());
+        qrCodeData.setName(visitors.getUserAllowed().getName());
+        qrCodeData.setDocument(visitors.getUserAllowed().getDocument());
+        qrCodeData.setDocumentType(visitors.getUserAllowed().getDocumentType().getDescription());
+        qrCodeData.setVehicle(visitors.getUserAllowed().getVehicles().get(0).getVehicleTypeId().getDescription());
+        qrCodeData.setPlate(visitors.getUserAllowed().getVehicles().get(0).getPlate());
+        qrCodeData.setGeneratedDate(LocalDateTime.now());
+        qrCodeData.setStartDate(visitors.getUserAllowed().getAuthRanges().get(0).getInitDate().atStartOfDay());
+        qrCodeData.setEndDate(visitors.getUserAllowed().getAuthRanges().get(0).getEndDate().atTime(23, 59, 59));
+
+        QRCode_Entity qrCodeEntity=new QRCode_Entity();
+        qrCodeEntity.setQrCodeData(objectMapper.writeValueAsString(qrCodeData));
+        qrCodeEntity.setCreatedDate(LocalDateTime.now());
+        qrCodeEntity.setIsValid(true);
+        qrCodeEntity.setUserAllowed(modelMapper.map(visitors.getUserAllowed(), Users_AllowedEntity.class));
+        when(qrCodeRepository.save(any(QRCode_Entity.class))).thenReturn(qrCodeEntity);
+
+        QRCode_Entity result = visitorQRService.generateAndSaveQRForVisitor(visitors);
+        assertNotNull(result);
+        assertEquals(qrCodeEntity.getQrCodeData(), result.getQrCodeData());
+        assertEquals(qrCodeEntity.getIsValid(), result.getIsValid());
+
+        verify(qrCodeRepository).save(any(QRCode_Entity.class));
+    }
+
+    @Test
+    void validateQRCode() {
+    }
+
+    @Test
+    void getQRCodeById() {
+    }
+}
